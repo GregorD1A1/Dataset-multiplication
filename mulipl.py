5dd171abbcbaaa99ec4059c351e2e3808291f805
unknown <dudnikgrv@gmail.com>
2021-10-08 17:14:31 +0200

    zrobione, sprawdzic czy wszystkie augumentatore sa ok

--- mulipl.py
+++ mulipl.py
@@ -1,17 +1,18 @@
 import os
-import imgaug as ia
 import imgaug.augmenters as iaa
 import numpy as np
 import cv2
+from tqdm import tqdm
 
 
-class ImageLoader():
+class ImageLoader:
     def __init__(self):
         self.images = np.empty((0, 300, 300, 3), dtype=np.uint8)
 
     def load(self, input_dataset_dir):
         print('Loading images...')
-        for image_name in os.listdir(input_dataset_dir):
+        # tqdm jest po to, by rysować ładne skale ładowania
+        for image_name in tqdm(os.listdir(input_dataset_dir)):
             image = cv2.imread(os.path.join(input_dataset_dir, image_name))
             image = make_image_square(image)
             # changing image size to achieve similar image sizes to make image batch
@@ -36,13 +37,14 @@ def make_image_square(image):
         margin = np.zeros((size, margin_width, 3), dtype=np.uint8)
 
     new_image = np.concatenate((margin, image, margin), axis=axis)
+
     return new_image
 
 
 def save_images(images, output_dir):
     print('Saving images...')
-    for i, image in enumerate(images):
-        #image = cv2.resize(image, (128, 128))
+    for i, image in tqdm(enumerate(images)):
+        # image = cv2.resize(image, (128, 128))
         cv2.imwrite(f'{output_dir}/image_{i}.png', image)
 
 
@@ -53,22 +55,15 @@ def augument_and_append(images, *augmentation_fcns):
     for augmentation_fcn in augmentation_fcns:
         augmented_images = augmentation_fcn(images=images)
         new_images = np.append(new_images, augmented_images, axis=0)
+
     return new_images
 
 
 def augmentation_pipeline(images):
     print('Generating new images...')
-    
-    # flip
-    aug_fcn1 = iaa.Fliplr()
-    images = augument_and_append(images, aug_fcn1)
-
-    # lightening and darkening
-    aug_fcn1 = iaa.Add(50)
-    aug_fcn2 = iaa.Add(-50)
-    images = augument_and_append(images, aug_fcn1, aug_fcn2)
 
     # colorizing
+    print('operation 1/7...')
     aug_fcn1 = iaa.WithChannels(0, iaa.Add((-50, -30)))
     aug_fcn2 = iaa.WithChannels(0, iaa.Add((50, 30)))
     aug_fcn3 = iaa.WithChannels(1, iaa.Add((-50, -30)))
@@ -77,28 +72,44 @@ def augmentation_pipeline(images):
     aug_fcn6 = iaa.WithChannels(2, iaa.Add((50, 30)))
     images = augument_and_append(images, aug_fcn1, aug_fcn2, aug_fcn3, aug_fcn4, aug_fcn5, aug_fcn6)
 
-    # contrast change
-    aug_fcn1 = iaa.GammaContrast((1.0, 2.0))
-    images = augument_and_append(images, aug_fcn1)
-
-    # histagram equalization (whatever it is)
-    aug_fcn1 = iaa.BlendAlpha((0.4, 0.6), iaa.AllChannelsHistogramEqualization())
-    images = augument_and_append(images, aug_fcn1)
-
-    # perspective change
-    aug_fcn1 = iaa.PerspectiveTransform(scale=(0.07, 0.07))
-    images = augument_and_append(images, aug_fcn1)
-
     # rotation
+    print('operation 2/7...')
     aug_fcn1 = iaa.Affine(rotate=(15, 20), scale=(1.15, 1.25))
     aug_fcn2 = iaa.Affine(rotate=(-15, -20), scale=(1.15, 1.25))
     images = augument_and_append(images, aug_fcn1, aug_fcn2)
 
+    # lightening and darkening
+    print('operation 3/7...')
+    aug_fcn1 = iaa.Add(50)
+    aug_fcn2 = iaa.Add(-50)
+    images = augument_and_append(images, aug_fcn1, aug_fcn2)
+
+    # histagram equalization (whatever it is)
+    print('operation 4/7...')
+    aug_fcn1 = iaa.BlendAlpha((0.4, 0.6), iaa.AllChannelsHistogramEqualization())
+    images = augument_and_append(images, aug_fcn1)
+
+    # flip
+    print('operation 5/7...')
+    aug_fcn1 = iaa.Fliplr()
+    images = augument_and_append(images, aug_fcn1)
+
+    # contrast change
+    print('operation 6/7...')
+    aug_fcn1 = iaa.GammaContrast((1.0, 2.0))
+    images = augument_and_append(images, aug_fcn1)
+
+    # perspective change
+    print('operation 7/7...')
+    aug_fcn1 = iaa.PerspectiveTransform(scale=(0.07, 0.07))
+    images = augument_and_append(images, aug_fcn1)
+
     return images
 
 
+
 # variables
-input_dataset_dir = './dataset_dir'
+input_dataset_dir = './raw_images'
 output_dataset_dir = './dataset_mltpl'
 
 # images loading
